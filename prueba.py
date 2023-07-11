c560aca325f1889310ec8f91e56938305a91305f
tokyomain <tokyomain@duck.com>
2023-07-11 19:38:32 -0300

    - fun creada > ver_lista_empleados() - Accion ver_lista_empleados() incorporada a la funcionalidad del Menu. - fun creada asignar_empleado_noche - funcionalidad incorporada al menu principal - fun creada ver_empleado_noche - funcionalidad incorporada al menu principal

--- prueba.py
+++ prueba.py
@@ -1,22 +1,34 @@
 from claseEmpleado import *
 from funcionesAux import *
-
+'''
 oPedro = Empleado('Pedro')
 oJuan = Empleado('Juan')
 oMatias = Empleado('Matias')
 lista = []
 lista.append(oPedro)
 lista.append(oJuan)
-lista.append(oMatias)
+lista.append(oMatias)'''
 # Lo siguiente funciona correctamente retornando los nombres en formato str de los empleados:
 #for empleado in lista:
 #    print(empleado.__str__())
 
 
 SEMANA = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'] 
-LISTA_EMPLEADOS = ['Juan', 'Nito1', 'Nito2', 'Pocho', 'Matias', 'Diego', 'Marcelo', 'Paco', 'David']
+LISTA_EMPLEADOS = ['Juan', 'Nito', 'Ruben', 'Pocho', 'Matias', 'Diego', 'Marcelo', 'Paco', 'David']
+lista2 = sorted(LISTA_EMPLEADOS)
+print(lista2)
 
+'''
 diccionario = crear_dict_empleados(LISTA_EMPLEADOS)
 print(f'Diccionario: {diccionario}')
-
-print(oJuan)
+'''
+DICCIONARIO_OBJETOS = crear_dict_empleados(LISTA_EMPLEADOS)
+'''
+lista__ = lista_nombre_hs(DICCIONARIO_OBJETOS)
+print(lista__)
+# Como interactuar con los objectos:
+DICCIONARIO_OBJETOS['Pocho'].actualizar_horas_trabajadas(8)
+print(DICCIONARIO_OBJETOS['Pocho'].horas_trabajadas)
+DICCIONARIO_OBJETOS['Juan'].asignar_franco_semanal('Lunes')
+print(DICCIONARIO_OBJETOS['Juan'].obtener_franco_semanal())
+'''
